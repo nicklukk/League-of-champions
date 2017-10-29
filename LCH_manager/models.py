c89c51a5088265c2f9dc11793b40b514d67d9ea6
Nikolay <nicklukk98@gmail.com>
2017-10-29 15:44:41 +0200

    db allmost done

--- LCH_manager/models.py
+++ LCH_manager/models.py
@@ -14,10 +14,10 @@ class Country(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
-    country = models.ForeignKey(Country)
+    country = models.ForeignKey(Country, blank=True, default=None, null=True)
 
     def __str__(self):
-        return "%s %s" % (self.country.name, self.name)
+        return "%s" % (self.name)
 
     class Meta:
         verbose_name = "City"
@@ -33,22 +33,6 @@ class Position(models.Model):
     class Meta:
         verbose_name = "Position"
         verbose_name_plural = "Positions"
-
-
-class Player(models.Model):
-    name = models.CharField(max_length=28)
-    surname = models.CharField(max_length=28)
-    position = models.ForeignKey(Position, blank=True, default=None)
-    number = models.PositiveIntegerField(blank=True, default=None)
-    birthday = models.DateField(blank=True, default=None)
-    motherland = models.ForeignKey(Country, blank=True, default=None)
-
-    def __str__(self):
-        return "%s %s" % (self.name, self.surname)
-
-    class Meta:
-        verbose_name = "Player"
-        verbose_name_plural = "Players"
 
 
 class Coach(models.Model):
@@ -74,16 +58,14 @@ class Group(models.Model):
         verbose_name_plural = "Groups"
 
 
-
 class Team(models.Model):
     name = models.CharField(max_length=56)
-    city = models.ForeignKey(City, blank=True, default=None)
-    year_of_foundation = models.PositiveIntegerField(blank=True, default=None)
-    coach = models.ForeignKey(Coach, blank=True, default=None)
-    players = models.ManyToManyField(Player, blank=True, default=None)
-    captain = models.ForeignKey(Player, related_name='captain_for_team', blank=True, default=None)
+    city = models.ForeignKey(City, blank=True, default=None, null=True)
+    year_of_foundation = models.PositiveIntegerField(blank=True, default=None, null=True)
+    coach = models.ForeignKey(Coach, blank=True, default=None, null=True)
     basket_index = models.PositiveIntegerField(default=0)
-    group = models.ForeignKey(Group, blank=True, default=None)
+    group = models.ForeignKey(Group, blank=True, default=None, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True, default=None)
 
     def __str__(self):
         return "%s" % (self.name,)
@@ -91,6 +73,24 @@ class Team(models.Model):
     class Meta:
         verbose_name = "Team"
         verbose_name_plural = "Teams"
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=28)
+    surname = models.CharField(max_length=28)
+    position = models.ForeignKey(Position, blank=True, default=None, null=True)
+    team = models.ForeignKey(Team, blank=True, default=None, null=True)
+    number = models.PositiveIntegerField(blank=True, default=None, null=True)
+    is_a_captain = models.BooleanField(default=False)
+    birthday = models.DateField(blank=True, default=None, null=True)
+    motherland = models.ForeignKey(Country, blank=True, default=None, null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.surname)
+
+    class Meta:
+        verbose_name = "Player"
+        verbose_name_plural = "Players"
 
 
 class Match(models.Model):
