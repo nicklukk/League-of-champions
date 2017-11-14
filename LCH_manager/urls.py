d3b0ad85f6c2c4b72b4913ebc5480aea4d888dfa
Nikolay <nicklukk98@gmail.com>
2017-11-14 15:44:09 +0200

    html

--- LCH_manager/urls.py
+++ LCH_manager/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from LCH_manager import views
 
 urlpatterns = [
-    url(r'^landing/$', views.home_page, name='home_page'),
+    url(r'^home/$', views.home_page, name='home_page'),
+    url(r'^player/(?P<player_id>\w+)/$', views.player, name='player'),
+    url(r'^team/(?P<team_id>\w+)/$', views.team, name='team'),
+    url(r'^match/(?P<match_id>\w+)/$', views.match, name='match'),
+    url(r'^country/(?P<country_name>\w+)/$', views.country, name='country'),
+    url(r'^leader_board/$', views.leader_board, name='leader_board'),
 ]
--- LCH_manager/views.py
+++ LCH_manager/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+from .models import *
+
 
 def home_page(request):
-    return render(request, 'LCH_manager/hello_world.html', locals())
+    return render(request, 'LCH_manager/home_page.html', locals())
+
+
+def player(request, player_id):
+    player = Player.objects.get(id=player_id)
+    return render(request, 'LCH_manager/player.html', locals())
+
+
+def team(request, team_id):
+    team = Team.objects.get(id=team_id)
+    return render(request, 'LCH_manager/team.html', locals())
+
+
+def match(request, match_id):
+    match = Match.objects.get(id=match_id)
+    return render(request, 'LCH_manager/match.html', locals())
+
+
+def country(request, country_name):
+    country = Country.objects.get(name=country_name)
+    teams = Team.objects.filter(city__country=country)
+    return render(request, 'LCH_manager/country.html', locals())
+
+
+def leader_board(request):
+
+    return render(request, 'LCH_manager/leader_bord.html', locals())
