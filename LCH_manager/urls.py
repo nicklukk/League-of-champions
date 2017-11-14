0be83c3e435c7d0f4f3fa2427b697c1bb236b64b
Nikolay <nicklukk98@gmail.com>
2017-11-14 12:19:33 +0200

    before renaming

--- LCH_manager/urls.py
+++ LCH_manager/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from LCH_manager import views
 
 urlpatterns = [
-
+    url(r'^landing/$', views.home_page, name='home_page'),
 ]
--- LCH_manager/views.py
+++ LCH_manager/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_page(request):
+    return render(request, 'LCH_manager/hello_world.html', locals())
